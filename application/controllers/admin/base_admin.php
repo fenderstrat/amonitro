3641c4c88b4619111b2e91c065786841a161c8cc
fenderstrat <fulcrum@fulcrum-laptop.(none)>
2014-03-31 21:23:32 +0700

    add session

--- application/controllers/admin/base_admin.php
+++ application/controllers/admin/base_admin.php
@@ -5,5 +5,8 @@ class Base_admin extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		if($this->session->userdata('user') === null){
+			return redirect('dashboard/login');
+		}
 	}
 }

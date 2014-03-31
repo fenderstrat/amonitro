d00602004195586d98824a9fd0d95a358d6b0d96
fulcrum <fulcrum@fulcrum-laptop.(none)>
2014-03-31 21:01:06 +0700

    add .htaccess & sql

---
+++ amonitro.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 31, 2014 at 04:00 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `amonitro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikel`
+--
+
+CREATE TABLE IF NOT EXISTS `artikel` (
+  `artikel_id` bigint(12) NOT NULL AUTO_INCREMENT,
+  `judul` varchar(250) NOT NULL,
+  `isi` longtext NOT NULL,
+  `deskripsi` text NOT NULL,
+  `keyword` varchar(250) NOT NULL,
+  `status` varchar(8) NOT NULL,
+  `tanggal` date NOT NULL,
+  PRIMARY KEY (`artikel_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `artikel`
+--
+
+INSERT INTO `artikel` (`artikel_id`, `judul`, `isi`, `deskripsi`, `keyword`, `status`, `tanggal`) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Ut enim ad minim veniam', 'draft', '2014-03-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `kategori_id` int(12) NOT NULL AUTO_INCREMENT,
+  `kategori` varchar(250) NOT NULL,
+  `parent` int(12) NOT NULL,
+  PRIMARY KEY (`kategori_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `kategori`, `parent`) VALUES
+(1, 'Lorem', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_artikel`
+--
+
+CREATE TABLE IF NOT EXISTS `kategori_artikel` (
+  `kategori_artikel_id` int(12) NOT NULL AUTO_INCREMENT,
+  `artikel_id` bigint(12) NOT NULL,
+  `kategori_id` int(12) NOT NULL,
+  PRIMARY KEY (`kategori_artikel_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `kategori_artikel`
+--
+
+INSERT INTO `kategori_artikel` (`kategori_artikel_id`, `artikel_id`, `kategori_id`) VALUES
+(1, 1, 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
